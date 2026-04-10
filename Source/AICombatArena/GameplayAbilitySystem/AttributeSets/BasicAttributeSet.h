8058e1d830fee14314144344d6d9f8186f00db92
filippov_kl <filippov_kl@robo-avia.ru>
2026-04-10 17:37:33 +0300

    jjj

--- Source/AICombatArena/GameplayAbilitySystem/AttributeSets/BasicAttributeSet.h
+++ Source/AICombatArena/GameplayAbilitySystem/AttributeSets/BasicAttributeSet.h
@@ -46,4 +46,8 @@ public:
 	void OnRep_MaxStamina(const FGameplayAttributeData& OldValue) const;
 	
 	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	
+	virtual void PostGameplayEffectExecute(const struct FGameplayEffectModCallbackData& Data) override;
 };
